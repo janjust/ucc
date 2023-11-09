b01df95db517b4213c61055b6a424f9d6f45e013
Sergey Lebedev <sergeyle@nvidia.com>
2023-11-09 08:21:56 +0100

    TL/SHM: fix build with nvc

--- src/components/tl/shm/bcast/bcast.c
+++ src/components/tl/shm/bcast/bcast.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  *
  * See file LICENSE for terms.
  */
@@ -268,7 +268,7 @@ ucc_status_t ucc_tl_shm_bcast_init(ucc_base_coll_args_t *coll_args,
 {
     ucc_tl_shm_team_t    *team   = ucc_derived_of(tl_team, ucc_tl_shm_team_t);
     ucc_tl_shm_pp_bcast_t params = {.super        = {0},
-                                    .progress_alg = 0};
+                                    .progress_alg = BCAST_WW};
     ucc_tl_shm_task_t    *task;
     ucc_status_t          status;
 
